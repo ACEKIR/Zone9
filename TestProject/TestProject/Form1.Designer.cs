a0fcd48695acd519d58c188396412bb5c0cd71d0
Acekir <kekscss@gmail.com>
2019-09-17 13:37:09 +0400

    first

---
+++ TestProject/TestProject/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace TestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "выход";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.993007F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.007F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(525, 295);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label6, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.textBox4, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.textBox3, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(266, 25);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.434944F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.56506F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(255, 266);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(185, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "y";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(4, 24);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 238);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(131, 24);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(120, 238);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "вход";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 25);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.434944F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.56506F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(255, 266);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "широта";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "долгота";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(4, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 238);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(131, 24);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 238);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(237, 322);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "B,L ---> x,y";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(237, 345);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "B,L <--- x,y";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Zone 9";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TestProject/TestProject/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Выполнить подсчет
+            if (radioButton1.Checked) CalculateXY(); else CalculateBL();
+
+        }
+
+
+        private void CalculateXY()
+        {
+            double B = 0;
+            double L = 0;
+            double x = 0;
+            double y = 0;
+
+            clear();
+
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                if ((textBox1.Lines[i] != "") && (textBox2.Lines[i] != ""))
+                {
+                    B = double.Parse(textBox1.Lines[i]);
+                    L = double.Parse(textBox2.Lines[i]);
+                    x = getX(B, L);
+                    y = getY(B, L);
+                    textBox3.AppendText(x + "\r\n");
+                    textBox4.AppendText(y + "\r\n");
+                }
+            }
+        }
+
+        private void CalculateBL()
+        {
+            double B = 0;
+            double L = 0;
+            double x = 0;
+            double y = 0;
+
+            clear();
+
+            for (int i = 0; i < textBox3.Lines.Length; i++)
+            {
+                if ((textBox3.Lines[i] != "") && (textBox4.Lines[i] != ""))
+                {
+                    x = double.Parse(textBox3.Lines[i]);
+                    y = double.Parse(textBox4.Lines[i]);
+                    B = getB(x, y);
+                    L = getL(x, y);
+                    textBox1.AppendText(B + "\r\n");
+                    textBox2.AppendText(L + "\r\n");
+                }
+            }
+        }
+
+    
+        private double getX(double B, double L)
+        {
+
+            double x = 0;
+            double l = 0;
+            int n = 0;
+
+            B = B * Math.PI / 180;
+
+            n = (int)(6 + L) / 6;
+            l = (L - (3 + 6 * (n - 1))) / 57.29577951;
+            x = 6367558.4968 * B - Math.Sin(2 * B) * (16002.89 + 66.9607 * Math.Pow(Math.Sin(B), 2) + 0.3515 * Math.Pow(Math.Sin(B), 4) -
+                Math.Pow(l, 2) * (1594561.25 + 5336.535 * Math.Pow(Math.Sin(B), 2) + 26.790 * Math.Pow(Math.Sin(B), 4) + 0.149 * Math.Pow(Math.Sin(B), 6) +
+                Math.Pow(l, 2) * (672483.4 - 811219.9 * Math.Pow(Math.Sin(B), 2) + 5420 * Math.Pow(Math.Sin(B), 4) - 10.6 * Math.Pow(Math.Sin(B), 6) +
+                Math.Pow(l, 2) * (278194 - 830174 * Math.Pow(Math.Sin(B), 2) + 572434 * Math.Pow(Math.Sin(B), 4) - 16010 * Math.Pow(Math.Sin(B), 6) +
+                Math.Pow(l, 2) * (109500 - 574700 * Math.Pow(Math.Sin(B), 2)) + 863700 * Math.Pow(Math.Sin(B), 4) - 398600 * Math.Pow(Math.Sin(B), 6)))));
+
+            return x;
+        }
+
+        private double getY(double B, double L)
+        {
+        
+            double y = 0;
+            double l = 0;
+            int n = 0;
+
+            B = B * Math.PI / 180;
+
+            n = (int)(6 + L) / 6;
+            l = (L - (3 + 6 * (n - 1))) / 57.29577951;
+            y = (5 + 10 * n) * Math.Pow(10, 5) + l * Math.Cos(B) * (6378245 + 21346.1415 * Math.Pow(Math.Sin(B), 2) + 107.1549 * Math.Pow(Math.Sin(B), 4) +
+                0.5977 * Math.Pow(Math.Sin(B), 6) + Math.Pow(l, 2) * (1070204.16 - 2136826.66 * Math.Pow(Math.Sin(B), 2) + 17.98 * Math.Pow(Math.Sin(B), 4) - 11.99 * Math.Pow(Math.Sin(B), 6) +
+                Math.Pow(l, 2) * (270806 - 1523417 * Math.Pow(Math.Sin(B), 2) + 1327645 * Math.Pow(Math.Sin(B), 4) - 21701 * Math.Pow(Math.Sin(B), 6) +
+                Math.Pow(l, 2) * (79690 - 866190 * Math.Pow(Math.Sin(B), 2) + 1730360 * Math.Pow(Math.Sin(B), 4) - 945460 * Math.Pow(Math.Sin(B), 6)))));
+
+            return y;
+        }
+
+        private double getB(double x, double y)
+        {
+            double B = 0;
+            double B0 = 0;
+            double dB = 0;
+            double betta = 0;
+            double z0 = 0;
+            int n = 0;
+
+            n = (int)(y * Math.Pow(10, -6));
+            betta = (x / 6367558.4968);
+            B0 = betta + Math.Sin(2 * betta) * (0.00252588685 - 0.00001491860 * Math.Pow(Math.Sin(betta), 2) + 0.00000011904 * Math.Pow(Math.Sin(betta), 4));
+            z0 = (y - (10 * n + 5) * Math.Pow(10, 5)) / (6378245 * Math.Cos(B0));
+
+
+            dB = -(Math.Pow(z0, 2)) * Math.Sin(2 * B0) * (0.251684631 - 0.003369263 * Math.Pow(Math.Sin(B0), 2) + 0.000011276 * Math.Pow(Math.Sin(B0), 4) -
+                 (Math.Pow(z0, 2)) * (0.10500614 - 0.04559916 * Math.Pow(Math.Sin(B0), 2) + 0.00228901 * Math.Pow(Math.Sin(B0), 4) -
+                 0.00002987 * Math.Pow(Math.Sin(B0), 6) - Math.Pow(z0, 2) * (0.042858 - 0.025318 * Math.Pow(Math.Sin(B0), 2) + 0.014346 * Math.Pow(Math.Sin(B0), 4) -
+                 0.001264 * Math.Pow(Math.Sin(B0), 6) - Math.Pow(z0, 2) * (0.01672 - 0.00630 * Math.Pow(Math.Sin(B0), 2) + 0.01188 * Math.Pow(Math.Sin(B0), 4) -
+                 0.00328 * Math.Pow(Math.Sin(B0), 6)))));
+
+            B = B0 + dB;
+            return B * 180 / Math.PI;
+        }
+
+        private double getL(double x, double y)
+        {
+            double l = 0;
+            double L = 0;
+            double z0 = 0;
+            double B0 = 0;
+            double betta = 0;
+            int n = 0;
+
+            n = (int)(y * Math.Pow(10, -6));
+            betta = (x / 6367558.4968);
+            B0 = betta + Math.Sin(2 * betta) * (0.00252588685 - 0.00001491860 * Math.Pow(Math.Sin(betta), 2) + 0.00000011904 * Math.Pow(Math.Sin(betta), 4));
+            z0 = (y - (10 * n + 5) * Math.Pow(10, 5)) / (6378245 * Math.Cos(B0));
+
+            l = z0 * (1 - 0.0033467108 * Math.Pow(Math.Sin(B0), 2) - 0.0000056002 * Math.Pow(Math.Sin(B0), 4) - 0.0000000187 * Math.Pow(Math.Sin(B0), 6) -
+                Math.Pow(z0, 2) * (0.16778975 + 0.16273586 * Math.Pow(Math.Sin(B0), 2) - 0.00052490 * Math.Pow(Math.Sin(B0), 4) - 0.00000846 * Math.Pow(Math.Sin(B0), 6) -
+                Math.Pow(z0, 2) * (0.0420025 + 0.1487407 * Math.Pow(Math.Sin(B0), 2) + 0.0059420 * Math.Pow(Math.Sin(B0), 4) - 0.0000150 * Math.Pow(Math.Sin(B0), 6) -
+                Math.Pow(z0, 2) * (0.01225 + 0.09477 * Math.Pow(Math.Sin(B0), 2) + 0.03282 * Math.Pow(Math.Sin(B0), 4) - 0.00034 * Math.Pow(Math.Sin(B0), 6) -
+                Math.Pow(z0, 2) * (0.0038 + 0.0524 * Math.Pow(Math.Sin(B0), 2) + 0.0482 * Math.Pow(Math.Sin(B0), 4) + 0.0032 * Math.Pow(Math.Sin(B0), 6))))));
+
+            L = 6 * (n - 0.5) / 57.29577951 + l;
+            return L * 180 / Math.PI;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Очистить поля
+            clear();
+        }
+
+        private void clear()
+        {
+            if (radioButton1.Checked)
+            {
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+
+
+    }
+}
